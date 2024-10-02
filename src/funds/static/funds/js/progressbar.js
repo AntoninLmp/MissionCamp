d4a02b8942f8fb0f33856dedbd0a6758300322a0
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2024-10-02 21:38:21 +0200

    feat(index): Json + progressbar

--- src/funds/static/funds/js/progressbar.js
+++ src/funds/static/funds/js/progressbar.js
@@ -1,4 +1,4 @@
-const moneyCollected = 150; // montant recolté
+let moneyCollected = 0; // montant recolté
 const moneyGoal = 5000; // montant objectif
 
 let totalGoal = document.getElementById('total-goal');
@@ -6,32 +6,41 @@ let totalCollected = document.getElementById('total-collected');
 let progressBar = document.getElementById('progressbarMoney');
 let descriptionBar = document.getElementById('descriptionBar');
 
-let pourcentage = (moneyCollected / moneyGoal) * 100;
-
-progressBar.style.width = pourcentage + "%";
-progressBar.innerHTML = pourcentage + "%";
-descriptionBar.innerHTML = "Il y a " + pourcentage + "% de l'objectif collecté";
-
-totalCollected.innerHTML = moneyCollected + "€"; 
-totalGoal.innerHTML = moneyGoal + "€";
-
+// Liste des extrajobs chargé avec un fichier json  
 document.addEventListener('DOMContentLoaded', function() {
-   fetch('static/funds/json/data.json')
+    fetch('static/funds/json/data.json')
     .then(response => response.json())
     .then(data => {
+        console.log(data);
         const table = document.querySelector("#data_extrajobs tbody"); 
         data.extrajobs.forEach( extrajob => {
             console.log(extrajob);
             const row = `
-                <tr>
-                    <th>${extrajob.date}</th>
-                    <td>${extrajob.description}</td>
-                    <td>${extrajob.personnes} pers.</td>
-                    <td>${extrajob.montant}€</td>
-                </tr>
+            <tr>
+            <th>${extrajob.date}</th>
+            <td>${extrajob.description}</td>
+            <td>${extrajob.personnes} pers.</td>
+            <td>${extrajob.montant}€</td>
+            </tr>
             `;
             table.innerHTML += row;
+            moneyCollected += extrajob.montant;
         });
+       updateMoneyCollected();
+       totalGoal.innerHTML = moneyGoal + "€";
     })
     .catch(error => console.log('Erreur : ' + error)); 
 });
+
+
+function updateProgressBar(moneyCollected) {
+    let pourcentage = ((moneyCollected / moneyGoal) * 100).toFixed(2);
+    progressBar.style.width = pourcentage + "%";
+    progressBar.innerHTML = pourcentage + "%";
+    descriptionBar.innerHTML = "Il y a " + pourcentage + "% de l'objectif collecté";
+}
+
+function updateMoneyCollected() {
+    totalCollected.innerHTML = moneyCollected + "€";
+    updateProgressBar(moneyCollected);
+}
